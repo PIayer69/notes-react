e3f349f2b1ef33241dca3250404bca284c93551b
piayer69 <maxmarsz1@gmail.com>
2022-08-15 14:18:26 +0200

    Added placeholder if user has no notes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,114 +1,112 @@
 import { useState, useEffect } from "react";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 
-import axiosInstance from '../Axios';
-import '../Navbar';
+import axiosInstance from "../Axios";
+import "../Navbar";
 import Navbar from "../Navbar";
 import Backdrop from "./components/Backdrop";
 import NewNote from "./components/NewNote";
 import NotePreview from "./components/NotePreview";
 import Notes from "./components/Notes";
 
+const Home = ({ api_url }) => {
+  const [notes, setNotes] = useState([]);
+  const [notePreview, setNotePreview] = useState(false);
+  const [noteId, setNoteId] = useState(1);
 
-const Home = ({api_url}) => {
-    const [notes, setNotes] = useState([])
-      const [notePreview, setNotePreview] = useState(false);
-      const [noteId, setNoteId] = useState(1);
+  const getNotes = () => {
+    axiosInstance.get("notes/").then((res) => {
+      if (typeof res.data.notes !== "undefined") setNotes(res.data.notes);
+    });
+  };
 
-      const getNotes = () => {
-        axiosInstance
-        .get('notes/')
-        .then(res => {
-          console.log(res)
-          setNotes(res.data.notes);
-        })
-      }
+  // Getting notes from backend
+  useEffect(() => {
+    getNotes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-      // Getting notes from backend
-      useEffect(() => {
-        getNotes();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
-      
+  const toggleNotePreview = (id) => {
+    setNotePreview(!notePreview);
+    setNoteId(id);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-      const toggleNotePreview = (id) => {
-        setNotePreview(!notePreview)
-        setNoteId(id)
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
+  const deleteNote = (id) => {
+    axiosInstance.delete("notes/" + id + "/");
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
+  const newNote = (note) => {
+    axiosInstance
+      .post("notes/", {
+        body: note.body,
+      })
+      .then((res) => {
+        note = {
+          id: res.data.id,
+          body: res.data.body,
+          created: res.data.created,
+        };
+        setNotes((prev) => [note, ...prev]);
       });
-      }
-    
+  };
 
-      const deleteNote = (id) => {
-        axiosInstance
-        .delete('notes/' + id + '/');
-        setNotes(notes.filter((note) => note.id !== id));
-      }
-    
-
-      const newNote = (note) => {
-        axiosInstance
-        .post('notes/', {
-          'body': note.body
-        })
-        .then(res => {
-          note = {
-            'id': res.data.id,
-            'body': res.data.body,
-            'created': res.data.created
-          }
-          setNotes([...notes, note]);
-        });
-      }
-    
-
-      const editNote = (id, body) => {
-        axiosInstance
-        .put('notes/' + id + '/', {
-          'body': body
-        })
-        .then(res => {
-          const updated_note = {
-            'id': res.data.id,
-            'body': res.data.body,
-            'created': res.data.created
-          }
-          setNotes(() => {
-            return(notes.map((note) => {
-              if(note.id === id){
-                note.body = updated_note.body
-              }
-              return note
-            }));
+  const editNote = (id, body) => {
+    axiosInstance
+      .put("notes/" + id + "/", {
+        body: body,
+      })
+      .then((res) => {
+        const updated_note = {
+          id: res.data.id,
+          body: res.data.body,
+          created: res.data.created,
+        };
+        setNotes(() => {
+          return notes.map((note) => {
+            if (note.id === id) {
+              note.body = updated_note.body;
+            }
+            return note;
           });
         });
-      }
-    
+      });
+  };
 
-      return (
-        <>
-          {localStorage.getItem('access_token') ? null : <Navigate to='/login/' />}
-          <Navbar/>
-          <div className="container">
-            <NewNote onAdd={newNote} />
-            {
-              notes ?
-              <Notes notes={notes} onDelete={deleteNote} onEdit={editNote} onPreview={toggleNotePreview}/>
-              :
-              'There are no notes available'
-            }
-            {
-              notePreview && 
-              <>
-                <Backdrop />
-                <NotePreview note={notes.filter((note) => note.id === noteId)[0]} onClose={() => setNotePreview(false)}/>
-              </>
-            }
+  return (
+    <>
+      {localStorage.getItem("access_token") ? null : <Navigate to="/login/" />}
+      <Navbar />
+      <div className="container">
+        <NewNote onAdd={newNote} />
+        {notes.length ? (
+          <Notes
+            notes={notes}
+            onDelete={deleteNote}
+            onEdit={editNote}
+            onPreview={toggleNotePreview}
+          />
+        ) : (
+          <div className="placeholder rectangle padding center">
+            It is empty... <br></br>Too empty
           </div>
-        </>
-      );
-}
+        )}
+        {notePreview && (
+          <>
+            <Backdrop />
+            <NotePreview
+              note={notes.filter((note) => note.id === noteId)[0]}
+              onClose={() => setNotePreview(false)}
+            />
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
